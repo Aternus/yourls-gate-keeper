43b157113765c3ec34af4dfa9fcc12a396d31ff2
Aternus <kiril@atern.us>
2025-07-02 22:44:40 +0300

    FEAT: refactor - extract config constants - added protection against hacky code execution

--- README.md
+++ README.md
@@ -1,24 +1,23 @@
-# admin-yourls-recaptcha-v3
-Secure YOURLS Admin with reCaptcha V3
+# yours-gate-keeper
 
-Description
------------
-Protect YOURLS admin with reCaptcha v3. The other plugins for admin recaptcha protection was with v2 and it didnt quite work. This should help protect your admin.
+Secure YOURLS Admin Area
 
-Installation
-------------
-1. Copy the project to /user/plugins.
+## Description
+
+Protect YOURLS admin with with reCaptcha v3.
+
+## Installation
+
+1. Copy the project to `/user/plugins` or `git clone`
 2. Obtain reCaptcha v3 key at [Google reCaptcha](https://www.google.com/recaptcha/admin)
-3. In plugin.php, please insert your keys RECAPTCHAV3SITEKEY and  RECAPTCHAV3SECRETKEY
-4. Go to the admin->manage plugins and activate this plugin (reCaptcha v3 for YOURLS Admin).
-5. Done. If you go to /admin/ page you should see the reCaptcha logo at the bottom right.
+3. In `config.php`, your keys `GATE_KEEPER_RECAPTCHA_V3_SITE_KEY` and `GATE_KEEPER_RECAPTCHA_V3_SECRET_KEY`
+4. Go to the Admin -> Manage Plugins and activate the plugin (Gate Keeper).
+5. Done. If you go to the `/admin/` page you should see the reCaptcha logo at the bottom right.
 
-License
--------
+## License
+
 MIT license
 
-Contact
--------
-Website: https://do.my
+## Contact
 
-[![Listed in Awesome YOURLS!](https://img.shields.io/badge/Awesome-YOURLS-C5A3BE)](https://github.com/YOURLS/awesome-yourls/)
+Website: https://atern.us
--- plugin.php
+++ plugin.php
@@ -1,38 +1,47 @@
 <?php
 /*
-Plugin Name: reCAPTCHA v3 for YOURLS Admin
-Plugin URI: https://github.com/axilaris/admin-yourls-recaptcha-v3/
+Plugin Name: Gate Keeper
+Plugin URI: https://github.com/Aternus/yourls-gate-keeper
 Description: Adds reCAPTCHA v3 to the YOURLS Admin login form.
 Version: 1.0
-Author: Firdaus Mah
-Author URI: https://do.my/
+Author: Kiril Reznik
+Author URI: https://atern.us/
 */
 
-// reCAPTCHA V3 Site Key
-define("recaptchaV3SiteKey", RECAPTCHAV3SITEKEY );
+// No direct call
+if (!defined('YOURLS_ABSPATH')) {
+    die();
+}
 
-// reCAPTCHA V3 Secret Key
-define("recaptchaV3SecretKey",  RECAPTCHAV3SECRETKEY);
+if (!defined('GATE_KEEPER_RECAPTCHA_V3_SITE_KEY')) {
+    return;
+}
+if (!defined('GATE_KEEPER_RECAPTCHA_V3_SECRET_KEY')) {
+    return;
+}
 
 // reCAPTCHA script to the head section of the HTML file
 yourls_add_action('html_head', 'recaptcha_v3_html_head');
-function recaptcha_v3_html_head() {
-    echo '<script src="https://www.google.com/recaptcha/api.js?render=' . recaptchaV3SiteKey . '"></script>';
+function recaptcha_v3_html_head()
+{
+    echo '<script src="https://www.google.com/recaptcha/api.js?render=' . GATE_KEEPER_RECAPTCHA_V3_SITE_KEY . '"></script>';
 }
 
 // reCAPTCHA widget to the YOURLS admin login form
 yourls_add_action('login_form_bottom', 'recaptcha_v3_login_form');
-function recaptcha_v3_login_form() {
+function recaptcha_v3_login_form()
+{
     echo '<div id="recaptcha"></div>';
     echo '<input type="hidden" name="token" id="tokenInput">';
 }
 
 // Initialize reCAPTCHA widget
 yourls_add_action('login_form_end', 'recaptcha_v3_inject_script');
-function recaptcha_v3_inject_script() {
+function recaptcha_v3_inject_script()
+{
     echo '<script>
         grecaptcha.ready(function() {
-            grecaptcha.execute(\'' . recaptchaV3SiteKey . '\', {action: \'submit\'}).then(function(token) {
+            grecaptcha.execute(\'' . GATE_KEEPER_RECAPTCHA_V3_SITE_KEY . '\', {action: \'submit\'}).then(function(token) {
                 document.getElementById(\'tokenInput\').value = token;
             });
         });
@@ -42,26 +51,31 @@ function recaptcha_v3_inject_script() {
 
 // Initialize reCAPTCHA widget and verify user's response
 yourls_add_action('pre_login_username_password', 'recaptcha_v3_validation');
-function recaptcha_v3_validation() {
+function recaptcha_v3_validation()
+{
     $token = $_POST['token'];
 
     // call curl to POST request
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL,"https://www.google.com/recaptcha/api/siteverify");
+    curl_setopt($ch, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
     curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('secret' => recaptchaV3SecretKey, 'response' => $token)));
+    curl_setopt(
+        $ch,
+        CURLOPT_POSTFIELDS,
+        http_build_query(array('secret' => GATE_KEEPER_RECAPTCHA_V3_SECRET_KEY, 'response' => $token))
+    );
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $response = curl_exec($ch);
     curl_close($ch);
     $arrResponse = json_decode($response, true);
 
     // verify the response
-    if($arrResponse["success"] == '1' && $arrResponse["score"] >= 0.5) {
+    if ($arrResponse["success"] == '1' && $arrResponse["score"] >= 0.5) {
         // reCAPTCHA succeeded
         return true;
     } else {
         // reCAPTCHA failed
-        yourls_login_screen( $error_msg = 'reCAPTCHA verification failed' );
+        yourls_login_screen($error_msg = 'reCAPTCHA verification failed');
         yourls_die('reCAPTCHA verification failed. Please try again.');
         return false;
     }
